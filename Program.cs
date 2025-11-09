2bac22363776739a720b584a482520039a1e53ff
Ahmet Tarik Sen <ahmettariksen12@gmail.com>
2025-11-09 22:45:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrunStokKayitUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun u = new Urun()
+            {
+                urunAd = "EKMEK",
+                urunFiyat = 10.5M,
+                urunStok = 400
+            };
+            u.ıner[0] = new ınerType()
+            {
+                kullanımYerleri = "SULU YEMEKLER"
+            };
+            u.urunEkle(u);
+
+            Urun u1 = new Urun()
+            {
+                urunAd = "SÜT",
+                urunFiyat = 25.75M,
+                urunStok = 700
+            };
+            u1.urunEkle(u1);
+            u.urunListele();
+
+            Console.WriteLine("TEKLİ LİSTELEME İÇİN Q HARFİNE BASINIZ");
+            
+            string cevap = Console.ReadLine().ToUpper();
+            if (cevap == "Q")
+                Console.Clear();
+            u.ıdListele();
+        }
+    }
+}
---
+++ Urun.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrunStokKayitUygulamasi
+{
+    internal class Urun
+    {
+        #region SANAL DATABESİMİZ (verileri tutacağımız bölge)
+        public static Dictionary<int, Urun> sanalDB = new Dictionary<int, Urun>();
+        #endregion
+
+        #region FİELDLARIMIZ
+        public string urunAd { get; set; }
+        #endregion
+
+        #region KAPSÜLLENECEK FİELDLARIMIZ
+        private int _urunID;
+        public int urunID
+        {
+            get
+            {
+                return _urunID;
+            }
+            private set
+            {
+                _urunID = value;
+            }
+        }
+        private int _urunStok;
+        public int urunStok
+        {
+            get
+            {
+                return _urunStok;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("NEGATİF DEĞERLERDE STOK KAYDI YAPILMAZ");
+                }
+                else
+                {
+                    _urunStok = value;
+                }
+            }
+        }
+        private decimal _urunFiyat;
+        public decimal urunFiyat
+        {
+            get
+            {
+                return _urunFiyat;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("NEGATİF FİYAT KAYDI YAPILMAZ");
+                }
+                else
+                {
+                    _urunFiyat = value;
+                }
+            }
+        }
+        #endregion
+
+        #region INERTYPE
+        public ınerType[] ıner;
+
+        #endregion
+
+        #region YAPICI METOT (uygulama ilk çalıştığı zaman burası devreye girecek)
+        public Urun()
+        {
+            this.urunID = ıdUret();
+            ıner = new ınerType[10];
+        }
+        #endregion
+
+        #region ID ÜRETEN METODUMUZ
+        private int ıdUret()
+        {
+            Random rnd = new Random();
+            int ıd;
+            do
+            {
+                ıd = rnd.Next(1, 100);
+            }
+            while (sanalDB.ContainsKey(ıd)); // aynı ıd atandığı zaman döngüden çıkacak
+            return ıd;
+        }
+        #endregion
+
+        #region URUN EKLEME METODUMUZ
+        public void urunEkle(Urun U)
+        {
+            if (U == null || string.IsNullOrEmpty(U.urunAd))
+            {
+                Console.WriteLine("ÜRÜN ADI BİLGİSİ EKSİK!");
+                return;
+            }
+            if (sanalDB.ContainsKey(U.urunID))
+            {
+                Console.WriteLine("BU ÜRÜN ZATEN KAYITLI!");
+            }
+            else
+            {
+                sanalDB.Add(U.urunID, U); // key, value
+                Console.WriteLine($"ÜRÜN EKLENDİ {U.urunAd} (ID : {U.urunID})");
+            }
+        }
+        #endregion
+
+        #region ÜRÜNLERİ LİSTELEME METODUMUZ
+        public void urunListele()
+        {
+            Console.WriteLine("\n---ÜRÜN LİSTESİ---");
+            foreach (var urun in sanalDB.Values)
+            {
+                Console.WriteLine($"ID: {urun.urunID} | Ad: {urun.urunAd} | Stok: {urun.urunStok} " +
+                    $"| Fiyat: {urun.urunFiyat}₺");
+            }
+        }
+
+        #endregion
+
+        #region ID YE GÖRE LİSTELEME
+        public void ıdListele()
+        {
+            Console.Write("LİSTELEMEK İSTEDİĞİNİZ ÜRÜNÜN ID SINI GİRİNİZ :");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+                if (sanalDB.ContainsKey(id))
+                {
+                    Urun u = sanalDB[id];
+                    Console.WriteLine($"\n---{u.urunID} ID NUMARALI ÜRÜNÜN BİLGİLERİ---");
+                    Console.WriteLine($"{u.urunAd} | {u.urunStok} | {u.urunFiyat}");
+                }
+                else
+                {
+                    Console.WriteLine("YANLIŞ ID GİRDİNİZ!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("LÜTFEN GEÇERLİ BİR SAYI GİRİNİZ!");
+            }
+        }
+        #endregion
+    }
+}
